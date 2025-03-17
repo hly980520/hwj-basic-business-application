be71b3dda48a91fc2c2d5a500a079704ea5a1437
peng.huang <peng.huang@hstong.com>
2025-03-17 18:00:40 +0800

    V1.0.0: 初始化项目

---
+++ src/main/java/com/hwj/basic/business/application/constant/DubboConst.java
@@ -0,0 +1,10 @@
+package com.hwj.basic.business.application.constant;
+
+/**
+ * @Program: hwj-user-center
+ * @Description: dubbo 常量类
+ * @author: wenjing.huang
+ * @since: 2025-03-14 15:02:10
+ */
+public class DubboConst {
+}
